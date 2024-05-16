04c6ab0459953cd84a0c5fc1ecd75be10eed3481
MichiOS <malagongasca@ciencias.unam.mx>
2024-05-16 05:22:02 -0600

    Maso ya

---
+++ src/GWO.py
@@ -0,0 +1,94 @@
+from Wolf import Wolf
+import random
+import numpy as np
+import time
+
+
+class GWO:
+
+    def __init__(self, population_size, iterations, train_set, train_target, random_seed=time.time()):
+        self.dimensions = len(train_set[0])
+        self.population_size = population_size
+        self.iterations = iterations
+        self.rand = random.Random(random_seed)
+
+        # Inicializamos la población
+        self.population = [Wolf(train_set, train_target, self.rand.randint(0, 10000)) for _ in range(population_size)]
+
+        # Datos que utilizaremos para ajustar parametros
+        self.train_set = train_set
+        self.train_target = train_target
+
+        # Jerarquia de la poblacion
+        self.alpha = None
+        self.beta = None
+        self.delta = None
+
+        self.set_hierarchy()
+
+
+    def initialize_population(self):
+        for individual in self.population:
+            individual.random_position()
+
+    def evaluate_population(self):
+        for individual in self.population:
+            individual.evaluate()
+
+    # Establecemos la jerarquia de la población
+    def set_hierarchy(self):
+
+        self.evaluate_population()
+
+        # Ahora tomamos al mejor como el alfa, al segundo como beta, tercero como delta y todos los demas son omegas
+        self.alpha = self.population[-1]
+        self.beta = self.population[-2]
+        self.delta = self.population[-3]
+
+    def hunt(self):
+        # Realizamos los ciclos especificados
+        for iteration in range(self.iterations):
+
+            # a decrementa paulatinamente de 2 a 0
+            a = 2 - iteration * (2 / self.iterations)
+
+            # Actualizamos la posicion de todos los miembros de la población
+            for wolf in range(self.population_size):
+
+                # Ecuación 3.3
+
+                A_1 = 2 * a * random.random() - a
+                A_2 = 2 * a * random.random() - a
+                A_3 = 2 * a * random.random() - a
+
+                # Ecuación 3.4
+
+                C_1 = 2 * random.random()
+                C_2 = 2 * random.random()
+                C_3 = 2 * random.random()
+
+                X_1 = [0 for _ in range(self.dimensions)]
+                X_2 = [0 for _ in range(self.dimensions)]
+                X_3 = [0 for _ in range(self.dimensions)]
+                X_t_prime = [0 for _ in range(self.dimensions)]
+
+                # Ecuaciones 3.5, 3.6 y 3.7
+                for dim in range(self.dimensions):
+                    X_1[dim] = self.alpha.pos[dim] - A_1 * abs(
+                        C_1 * self.alpha.pos[dim] - self.population[wolf].pos[dim])
+                    X_2[dim] = self.beta.pos[dim] - A_2 * abs(C_2 * self.beta.pos[dim] - self.population[wolf].pos[dim])
+                    X_3[dim] = self.delta.pos[dim] - A_3 * abs(
+                        C_3 * self.delta.pos[dim] - self.population[wolf].pos[dim])
+                    X_t_prime[dim] = (X_1[dim] + X_2[dim] + X_3[dim]) / 3
+
+                old_eval = self.population[wolf].fitness
+                old_position = self.population[wolf].pos
+                self.population[wolf].update_position = X_t_prime
+                new_eval = self.population[wolf].fitness
+                if old_eval > new_eval:
+                    self.population[wolf].update_position = old_position
+
+            self.population = np.sort(self.population)
+            self.set_hierarchy()
+
+        return self.alpha
--- src/Wolf.py
+++ src/Wolf.py
@@ -6,21 +6,18 @@ from math import exp
 
 class Wolf:
 
-    def __init__(self, features, train_target, test_features, test_target, seed=time.time()):
+    def __init__(self, features, target, seed=time.time()):
         self.features = features
-        self.test_features = test_features
-        self.dimensiones = len(features)
+        self.dimensions = len(features) + 1
         self.function = accuracy_score
         # El valor mínimo y máximo que puede tomar un coeficiente
-        self.min, self.max = -1000, 1000
-        self.pos = [0 for _ in range(self.dimensiones + 1)]
+        self.min, self.max = -10, 10
+        self.pos = [0] * self.dimensions
         self.fitness = 0
         self.seed = seed
         self.random = random.Random(seed)
-        self.predictions = [0 * len(self.features)]
-        self.test_predictions = [0 * len(self.features)]
-        self.expected = train_target
-        self.test_expected = test_target
+        self.predictions = [0] * len(self.features)
+        self.expected = target
 
         # Inicializamos a la población
         self.random_position()
@@ -28,39 +25,32 @@ class Wolf:
     def __lt__(self, other):
         return self.fitness < other.fitness
 
+    def __eq__(self, other):
+        return self.fitness == other.fitness
+
     def __gt__(self, other):
         return self.fitness >= other.fitness
 
     # Inicializamos el gen de manera aleatoria
     def random_position(self):
-        self.pos = [self.random.random() for _ in range(self.dimensiones + 1)]
-
-    # Imprime los valores reales correspondientes a los bits
-    def show_values(self):
-        for d in range(len(self.pos)):
-            print(f"Posición en dimensión {d + 1}: {self.pos[d]}")
-        print(f"Evaluación: {self.fitness}")
+        self.pos = [(self.random.random() * (self.max - self.min) + self.min) for _ in range(self.dimensions)]
 
     # Evalúa nuestro gen en el conjunto de entranamiento (con sezgo)
-    def evaluate_train(self):
+    def evaluate(self):
+        self.calculate_probabilities()
         self.fitness = accuracy_score(self.expected, self.predictions)
         return self.fitness
-
-    # Evalúa nuestro gen en el conjunto de prueba (sin sezgo)
-    def evaluate_test(self):
-        test_error = accuracy_score(self.test_expected, self.test_predictions)
-        return test_error
 
     def calculate_probabilities(self):
         for p in range(len(self.features)):
             # Inicializamos la suma como el valor independiente
             val = self.pos[0]
-            for index in range(len(self.features)):
+            for index in range(len(self.features[0])):
                 # Sumamos el valor de la variable por el peso asociado (se desplaza 1 ya que tenemos el valor independiente)
-                val += self.features[index] * self.pos[index + 1]
+                val += self.features[p][index] * self.pos[index + 1]
             # Una vez que tengamos eso calculamos el valor de la prediccion
             exp_val = exp(val)
-            prob = exp_val / 1 + exp_val
+            prob = exp_val / (1 + exp_val)
             # Despues si la probabilidad es mayor a 50% consideramos que está prendido el valor y predecimos un positivo
             if prob >= .5:
                 self.predictions[p] = 1
@@ -69,4 +59,9 @@ class Wolf:
     
     def update_position(self, new_position):
         self.pos = new_position
-        self.function.set_vals(self.pos)
+
+    def get_position(self):
+        return self.pos
+
+    def __str__(self):
+        return f"Predicciones: {self.predictions}\nAptitud: {self.fitness}"
